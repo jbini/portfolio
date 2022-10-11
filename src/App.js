e055db87807eb36aa4cc8e8547d73463afcee85d
Jongbin Lim <rkwk0515@gmail.com>
2022-10-12 02:45:02 +0900

    1012

--- src/App.js
+++ src/App.js
@@ -9,22 +9,32 @@ let mainimg = main2
 function App() {
   return (
     <div className="main">
+      <div className='container'>
 
-      <div className='menu-container'>
-        <div className='menu-list-box'>
-          
+        <div className='menu-container'>
+
+          <div className='menu-list-box'>
+            <div className='menu-list-line'><h4>a</h4></div>
+            <div className='menu-list-line'>b</div>
+            <div className='menu-list-line'>c</div>
+            <div className='menu-list-line'>d</div>
+            <div className='menu-list-line'>e</div>
+          </div>
+          <div className='menu-address-box'>
+            
+          </div>
+
         </div>
-        <div className='menu-address-box'>
-          
+
+        <div className='main-cover-container'>
+
+          <div className='main-cover-imgbox'>
+              <img src={mainimg} alt='mainimg'></img>
+          </div>
+
         </div>
+
       </div>
-
-      <div className='main-cover-container'>
-        <div className='main-cover-imgbox'>
-          <img src={mainimg} alt='mainimg'></img>
-        </div>
-      </div>
-
     </div>
   );
 }

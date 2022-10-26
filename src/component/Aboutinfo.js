10cf06354286a5795a76c1c72f62f5a0e2b4e679
Jongbin Lim <rkwk0515@gmail.com>
2022-10-26 17:31:33 +0900

    1026

--- src/component/Aboutinfo.js
+++ src/component/Aboutinfo.js
@@ -20,6 +20,25 @@ export default function Aboutinfo() {
           <div className='about-cover-line-box'>
             <div className='about-cover-line'></div>
           </div>
+          <div className='about-cover-line-box2'>
+            <div className='about-cover-line2'></div>
+          </div>
+
+          <div className='about-line1'>
+            <div className='line-move'>
+              <h4>오늘 아침 메뉴는 김치볶음입니다.</h4>
+            </div>
+          </div>
+          <div className='about-line2'>
+            <div className='line-move'>
+              <h4>맛있는 식사하시길 바랍니다.</h4>
+            </div>
+          </div>
+          <div className='about-line3'>
+            <div className='line-move'>
+              <h4>출구는 오른쪽입니다.</h4>
+            </div>
+          </div>
 
         </div>
     );

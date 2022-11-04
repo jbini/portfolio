3743bfc60436dcd33868fa7e62c93fe22f2b3618
Jongbin Lim <rkwk0515@gmail.com>
2022-11-04 20:03:44 +0900

    1104

--- src/component/Projectinfo.js
+++ src/component/Projectinfo.js
@@ -1,9 +1,10 @@
 import '../css/Projectinfo.css';
 import main from '../img/main.jpg'
 import main2 from '../img/main2.jpg'
-import main22 from '../img/main2-2.jpg'
+import main2me from '../img/main2-2.jpg'
 
 let mainimg = main2
+let mainimg2 = main2me
 
 export default function Projectinfo() {
     return(
@@ -11,6 +12,10 @@ export default function Projectinfo() {
 
           <div className='project-cover-imgbox'>
               <img src={mainimg} alt='mainimg'></img>
+          </div>
+
+          <div className='project-cover-imgbox2'>
+              <img src={mainimg2} alt='mainimg2'></img>
           </div>
 
         </div>

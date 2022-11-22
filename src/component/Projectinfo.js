403e7df07281f037f5432b8f6e4134710565570c
Jongbin Lim <rkwk0515@gmail.com>
2022-11-23 00:45:47 +0900

    1123

--- src/component/Projectinfo.js
+++ src/component/Projectinfo.js
@@ -1,6 +1,7 @@
 import '../css/Projectinfo.css';
 import ReactPlayer from 'react-player';
 import eatmotion from '../video/Eatmotion.mp4';
+import eatmotion1 from '../img/eatmotion1.png'
 import $ from 'jquery';
 
 
@@ -10,6 +11,7 @@ export function videoPause() {
     $('#projectIframe1').get(0).pause();
 };
 
+let contentimg1 = eatmotion1
                     
 
 export default function Projectinfo() {
@@ -32,8 +34,21 @@ export default function Projectinfo() {
                 <label className='project-box-X1' for='project-dark'><h3>X</h3></label>
                 {/* <div className='project-box-dot4'></div> */}
                 <h3>INFORMATION</h3>
-                <div className='project-box-content1'></div>
+                <div className='project-box-content1'>
+                    <div className='project-box-content-img'><img src={contentimg1} alt='eatmotion'/></div>
+                    <div className='project-box-content-name1'><h4>Project Name</h4></div>
+                    <div className='project-box-content-name2'><h5>Eatmotion</h5></div>
+                    <div className='project-box-content-sdate1'><h4>Date</h4></div>
+                    <div className='project-box-content-sdate2'><h5>2021.12.03 ~ 2021.12.24</h5></div>
+                    <div className='project-box-content-back1'><h4>Back-end</h4></div>
+                    <div className='project-box-content-back2'><h5>Java, Javascript, AWS, MariaDB, MySQL</h5></div>
+                    <div className='project-box-content-front1'><h4>Front-end</h4></div>
+                    <div className='project-box-content-front2'><h5>ReactJS, HTML, CSS</h5></div>
+                    <div className='project-box-content-member1'><h4>Member</h4></div>
+                    <div className='project-box-content-member2'><h5>jbini, xljhax, dev-thug, delpotro96</h5></div>
+                </div>
             </label>
+
             {/* <div className='project-box1-up' id='projectboxup'>
                 <div className='project-box-dot1-2'></div>
                 <h3>INFORMATION</h3>

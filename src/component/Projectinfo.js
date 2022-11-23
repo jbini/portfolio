4317cb271b4c5499cc1b87a418e97dc92a1f0763
Jongbin Lim <rkwk0515@gmail.com>
2022-11-24 01:11:19 +0900

    -

--- src/component/Projectinfo.js
+++ src/component/Projectinfo.js
@@ -5,7 +5,6 @@ import eatmotion1 from '../img/eatmotion1.png'
 import $ from 'jquery';
 
 
-// export let projectI = document.getElementById('projectIframe1');
 
 export function videoPause() {
     $('#projectIframe1').get(0).pause();
@@ -18,9 +17,6 @@ export default function Projectinfo() {
     return(
         <div className='project-cover-container'>
 
-            {/* <input type='checkbox' name='boxclick1' id='box-click1' />
-            <input type='checkbox' name='boxclick2' id='box-click2' />
-            <input type='checkbox' name='boxclick3' id='box-click3' /> */}
             <label className='project-darkbg' id='project-darkbg1' for='project-dark' onClick={videoPause}></label>
 
             <label className='project-box1' id='projectbox' for='project-info'>
@@ -32,7 +28,6 @@ export default function Projectinfo() {
                 <div className='project-box-dot1'></div>
                 <div className='project-box-dot1-2'></div>
                 <label className='project-box-X1' for='project-dark'><h3>X</h3></label>
-                {/* <div className='project-box-dot4'></div> */}
                 <h3>INFORMATION</h3>
                 <div className='project-box-content1'>
                     <div className='project-box-content-img'><img src={contentimg1} alt='eatmotion'/></div>
@@ -59,11 +54,6 @@ export default function Projectinfo() {
                 </div>
             </label>
 
-            {/* <div className='project-box1-up' id='projectboxup'>
-                <div className='project-box-dot1-2'></div>
-                <h3>INFORMATION</h3>
-            </div> */}
-
             <label className='project-box2' id='projectbox' for='project-pre'>
                 <div className='project-box-effect1' id='project-box-effect2'></div>
                 <div className='project-box-effect2' id='project-box-effect2'></div>
@@ -79,8 +69,6 @@ export default function Projectinfo() {
                     {/* <ReactPlayer id='projectIframe1' url='https://www.youtube.com/embed/qOqlykY3bWE' width="100%" height="100%" controls /> */}
                     <video id='projectIframe1' src={eatmotion} controls width="100%" height="100%" />
                 </div>
-                
-                
             </label>
 
             <label className='project-box3' id='projectbox' for='project-eva'>
@@ -93,7 +81,22 @@ export default function Projectinfo() {
                 <div className='project-box-dot3-2'></div>
                 <label className='project-box-X3' for='project-dark'><h3>X</h3></label>
                 <h3>EVALUATION</h3>
-                <div className='project-box-content3'></div>
+                <div className='project-box-content3'>
+                    <div className='project-box-content-epilogue1'><h4>협업을 하며..</h4></div>
+                    <div className='project-box-content-epilogue2'>
+                        <h5>프로젝트를 함께 할 때와 혼자 할 때의 차이는 분명했습니다.</h5>
+                        <h5>혼자 할 때 만큼의 여유는 없었지만 책임감을 갖고 몰두할 수 있었습니다.</h5>
+                        <h5>막히는 부분이 나오면 팀원에게 해결법이나 대안을 받아 실력 향상에 도움이 됐습니다.</h5>
+                        <h5>작업 중간에 서로 컨디션 체크를 하고 일상 얘기를 하며 숨을 돌리면 작업 효율이 좋아지기도 했습니다.</h5>
+                    </div>
+                    <div className='project-box-content-epilogue3'><h4>좋은 개발자 좋은 코드</h4></div>
+                    <div className='project-box-content-epilogue4'>
+                        <h5>개발자에게 협업이 필수인 만큼 커뮤니케이션 능력이 중요합니다.</h5>
+                        <h5>123</h5>
+                        <h5>345</h5>
+                        <h5>234</h5>
+                    </div>
+                </div>
             </label>
 
         </div>
